c0b6ceee007226c46174b7211c7fb3914ac9c844
Tyler Tracy <tylertracy1999@gmail.com>
2018-08-23 19:08:34 -0500

    Setting up project 2

---
+++ Project2/Model.java
@@ -0,0 +1,28 @@
+class Model {
+  int turtle_x;
+  int turtle_y;
+  int dest_x;
+  int dest_y;
+
+  Model() {
+
+  }
+
+  public void update() {
+    if (this.turtle_x < this.dest_x) {
+      this.turtle_x += Math.min(4, dest_x - turtle_x);
+    } else if (this.turtle_x > this.dest_x) {
+      this.turtle_x -= Math.min(4, turtle_x - dest_x);
+    }
+    if (this.turtle_y < this.dest_y) {
+      this.turtle_y += Math.min(4, dest_y - turtle_y);
+    } else if (this.turtle_y > this.dest_y) {
+      this.turtle_y -= Math.min(4, turtle_y - dest_y);
+    }
+  }
+
+  public void setDestination(int x, int y) {
+    this.dest_x = x;
+    this.dest_y = y;
+  }
+}
