7bdad120c7d9e943b9e9ef0c2e92d5dbbbef1aa7
Tyler Tracy <me@tylertracy.com>
2018-10-11 16:03:15 -0500

    init commit

--- Project4/build.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e -x
-javac Game.java View.java Controller.java Model.java Brick.java Json.java Mario.java Sprite.java Coin.java CoinBlock.java MapEditor.java Editor.java
-
-echo "Press WAD for movement and Shift to sprint"
-
